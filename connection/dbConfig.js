f851229b0126f8517d606ab11698f38120aa929a
gauravKothari <kotharigaurav6@gmail.com>
2025-02-24 14:53:50 +0530

    update mongodb atlas password

--- connection/dbConfig.js
+++ connection/dbConfig.js
@@ -1,3 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 export const url = process.env.MONGO_URI;
+
+// username : edusphere
+// password : edusphere@12345
+
+// mongodb+srv://edusphere:edusphere@12345@cluster0.70tox.mongodb.net/edusphere?retryWrites=true&w=majority&appName=Cluster0
